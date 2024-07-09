0f07369bfe67aecf372865d63b33478b16d650c7
Blake Heller <46772460+hellerblake@users.noreply.github.com>
2024-07-09 06:17:13 -0700

    update main page

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,9 +1,12 @@
-<div class="p-4 dark:text-white">
-  <div class="text-2xl font-bold">Hi there, I'm Blake 👋</div>
-  <div class="flex pt-3">
-    <div class="text-lg">
+<div class="p-4 dark:text-white w-full"> <div class="flex flex-col-reverse pt-3 md:flex-row">
+    <div class="text-lg my-auto">
+      <div class="text-2xl font-bold my-6">Hi there, I'm <span class="text-orange-500">Blake</span> 👋</div>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum dolor, hic adipisci consectetur tempore aspernatur, quidem iure nulla quis alias expedita error ullam fugiat culpa aperiam repellendus, corporis recusandae labore?
+      <div class="mt-6 flex gap-4 w-full text-center">
+        <img src="/youtube_logo.png" class="h-12 my-auto" alt="">
+        <img src="/github.png" class="h-16 my-auto" alt="">
+      </div>
     </div>
-    <div>right</div>
+    <div class="w-full"><img src="https://avatars.githubusercontent.com/u/46772460?u=b4bf1ba33e5136964697b48486a0c339d9a033ba&v=4" alt="Profile" class="rounded-full max-w-80 border border-white mx-auto"></div>
   </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 	import Hero from "$lib/components/Hero.svelte";
+	import RecentProjects from "$lib/components/RecentProjects.svelte";
 
 
 
 </script>
 
-<main>
-  <Hero/>
+<main class="flex flex-col md:w-2/3 w-full mx-auto">
+  <Hero />
+  <RecentProjects/>
 </main>
