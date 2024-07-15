767eadca47effe86cdd076c5c04764348f993dbc
Blake Heller <46772460+hellerblake@users.noreply.github.com>
2024-07-15 05:57:17 -0700

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<script>
+  import { page } from "$app/stores"
+</script>
+<div class="text-center w-full mt-24 text-5xl dark:text-white"> {$page.status} | <span class="text-3xl align-middle">{$page.error?.message}</span>  </div>
